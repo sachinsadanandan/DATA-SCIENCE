adaabbebcc27c6557800985645ef6ac0949b384f
sachinsadanandan <103180738+sachinsadanandan@users.noreply.github.com>
2022-09-25 23:40:36 -0700

    Add files via upload

---
+++ prgm 4-2.py
@@ -0,0 +1,4 @@
+import pandas as pd
+details = {'Name' : ['a','b','c','d'],'Age' : [24,25,26,27],}
+df = pd.DataFrame(details)
+print(df)
---
+++ prgm 4-3.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv('file1.csv')
+print(df.to_string())
